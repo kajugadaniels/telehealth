d55e85a43ad6e6d6c80151ccbeefee03caab8828
kajugadaniels <kajugadaniels@gmail.com>
2024-06-25 01:25:13 +0200

    Add roles

--- account/admin.py
+++ account/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from account.models import User
+from account.models import *
 
-class CustomUserAdmin(UserAdmin):
+class UserAdmin(UserAdmin):
     model = User
     list_display = ('name', 'phone_number', 'id_number', 'role')
     search_fields = ('name', 'phone_number', 'id_number', 'role')
     ordering = ('id',)
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Role)
