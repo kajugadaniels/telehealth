796d4a8bb835126187da3ea0a084ff36373ab729
kajugadaniels <kajugadaniels@gmail.com>
2024-06-25 13:36:45 +0200

    Retrieve patients

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,10 @@ app_name = 'base'
 
 urlpatterns = [
     path('dashboard', dashboard, name="dashboard"),
+
+    path('patients', getPatients, name='getPatients'),
+    path('patient/add', addPatient, name='addPatient'),
+    path('patient/<mrn>', getPatient, name='getPatient'),
+    path('patient/<mrn>/edit', editPatient, name='editPatient'),
+    path('patient/<mrn>/delete', deletePatient, name='deletePatient'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- base/views.py
+++ base/views.py
@@ -1,6 +1,33 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from base.models import *
 
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+@login_required
+def getPatients(request):
+    patients = Patient.objects.all().order_by('-created_at')
+
+    context = {
+        'patients': patients
+    }
+
+    return render(request, 'patients/index.html', context)
+
+@login_required
+def addPatient(request):
+    pass
+
+@login_required
+def getPatient(request, mrn):
+    pass
+
+@login_required
+def editPatient(request, mrn):
+    pass
+
+@login_required
+def deletePatient(request, mrn):
+    pass
